f86dd6d7998ca13bb8490012db22a3f0a07a341a
RstoykoMadeit <rstoyko@hawk.iit.edu>
2025-04-03 21:22:50 -0500

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  var form = document.querySelector('form');
+  var error = document.getElementById('speciesError');
+  
+  form.onsubmit = function(e) {
+    var human = document.getElementById('human').checked;
+    var turkey = document.getElementById('turkey').checked;
+    
+    if (!human && !turkey) {
+      error.textContent = "If not turkey nor human then who are you";
+      e.preventDefault();
+    }
+    else {
+      error.textContent = "";
+    }
+  };
+};
